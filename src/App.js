21e53596a3e664d4552e6db9818f458a53b25978
leejinwoo <leejinwoo1126@gmail.com>
2024-05-18 21:58:51 +0900

    feat: react router 생성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
+import {Outlet} from "react-router-dom";
 
-function App() {
-  return (
-    <>
-      <iframe width="480" height="270" src="//www.youtube.com/embed/lTRiuFIWV54" frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-    </>
-  );
+export default function App() {
+    return (
+        <Outlet/>
+    );
 }
 
-export default App;

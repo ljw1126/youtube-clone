4722b856d47f2f0240b47b244191349313cefe03
leejinwoo <leejinwoo1126@gmail.com>
2024-05-20 12:07:18 +0900

    refactor: header component file naming update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from "react";
-import Header from "./components/header";
+import Header from "./components/Header";
 import {Outlet} from "react-router-dom";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {YoutubeApiProvider} from "./context/YoutubeApiContext";
